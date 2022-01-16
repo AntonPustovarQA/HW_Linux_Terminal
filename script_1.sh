e6beb0c9d82abff53308f66f0ced11a027fd2450
AntonPustovarQA <Antonpustovarqa@gmail.com>
2022-01-17 01:26:26 +0200

    Add script_1.sh file

---
+++ script_1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd Folder_1
+mkdir folder_1 folder_2 folder_3
+cd folder_1
+touch filename_1.txt filename_2.txt filename_3.txt filename_4.json filename_5.json
+mkdir folder_1_1 folder_1_2 folder_1_3
+ls -la
+mv filename_1.txt filename_2.txt folder_1_2/
